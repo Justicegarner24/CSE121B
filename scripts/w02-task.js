474814cf3fef75b174d88096bd8b9c2ca81824e9
justicegarner24 <gar17042@byui.edu>
2023-11-11 23:51:43 -0700

    week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,33 +3,28 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
 let fullName = 'Justice Garner';
-let currentYear = '2023';
+let currentYear = 2023;
 let profilePicture = 'images/mypicture.jpg'
 
-
 /* Step 3 - Element Variables */
-
-const nameElement = document.getElementById ('name');
-const foodElement = document.getElementById ('food');
-const yearElement = document.querySelector ('#year');
-var imageElement = document.getElementById('image');
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('picture img');
 
 /* Step 4 - Adding Content */
-
-nameElement.innerHTML = '<strong>${fullName}<strong>';
-currentYear.textContent = '<strong>${currentYear}<strong>';
+nameElement.innerHTML = '<strong>Justice Garner</strong>';
 imageElement.setAttribute('src' , profilePicture);
-imageElement.setAttribute('alt' , 'Profile image of [ProfilePicture]');
-
-
+imageElement.setAttribute('alt' , 'Profile image of ${fullName}');
 
 /* Step 5 - Array */
-
-const favFoods = ['sushi', 'burgers', 'waffles', 'ribs'];
-
-
-
-
+const favFoods = [' sushi ', ' burgers ', ' waffles ', ' ribs '];
+foodElement.innerHTML = favFoods;
+const extraFoodItem = " Mac and cheese ";
+favFoods.push(extraFoodItem);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
